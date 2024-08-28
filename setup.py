a1b5e6bea7fac8bf3cbe8bc1b44bdd45b9706699
pkeorley <kolyaperedriy13@gmail.com>
2024-08-28 03:01:27 +0200

    chore: Release v0.5.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     author_email='pkeorley@gmail.com',
 
     url='https://github.com/pkeorley/python-lenv',
-    version="0.4.0",
+    version="0.5.0",
 
     license='MIT',
     license_files=['LICENSE'],
